af5e67c74348623c10d666fa061fe9928d2dd455
rishabhshukla <rishabh.shukla8@gmail.com>
2024-07-03 19:51:48 +0530

    Added the assignment of day3

--- M03-Day-03/Notes.md
+++ M03-Day-03/Notes.md
@@ -1,1 +1,6 @@
-## Day 3 - [where  between like in operator ]
+#### Day 3 - [where  between like in operator ]
+
+* where 
+
+- customer name should start with C and second character shoul be a,l
+    - select order_id , order_date , customer_name from orders where customer_name like 'C[al]%';
--- M03-Day-03/practise_d3.sql
+++ M03-Day-03/practise_d3.sql
@@ -26,9 +26,19 @@ select cast(GETDATE() as date) date_col ;
 
 -- Like operator 
 select order_id , order_date , customer_name from orders;
-
-
 select order_id , order_date , customer_name from orders where customer_name like 'Chris%'; 
 select order_id , order_date , customer_name from orders where customer_name like '%ld';
 
 select order_id , order_date , customer_name from orders where customer_name like '%ven%';
+select order_id , order_date , customer_name from orders where customer_name like '_l%';
+
+-- customer name should start with C and second character shoul be a,l
+select order_id , order_date , customer_name from orders where customer_name like 'C[al]%';
+select order_id , order_date , customer_name from orders where customer_name like 'C[a-d]%' order by customer_name;
+select order_id , order_date , customer_name from orders where customer_name like 'C[a-z]%';
+
+select order_id , order_date , customer_name from orders where order_id like 'CA-201[7-9]%' order by order_id;
+
+-- second chracter should not [^albo]
+select order_id , order_date , customer_name from orders where customer_name like 'C[^albo]%';
+select order_id , order_date , customer_name from orders where customer_name like 'C[^a-z]%';
